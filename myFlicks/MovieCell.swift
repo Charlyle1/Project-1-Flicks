18d115158d70d0c2298d3aff3944e2f5c68aab7d
Carly Burroughs <GREG@greg-burroughss-macbook.local>
2016-01-31 18:47:48 -0500

    Flicks

---
+++ myFlicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  myFlicks
+//
+//  Created by Carly Burroughs on 1/31/16.
+//  Copyright © 2016 Carly Burroughs. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
